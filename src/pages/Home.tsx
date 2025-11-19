496e5946957096197b008a884a68be31f4a0835b
Wildan Nur Awaludin <wildanelgato@gmail.com>
2025-11-19 14:02:56 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,9 @@ import ProjectCard from "@/components/ProjectCard";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import { ArrowRight } from "lucide-react
-import OrbitCircleText from "@/components/OrbitCircleText";
+import OrbitText from "@/components/OrbitText";
 
-<OrbitCircleText 
+<OrbitText 
   text="Welcome To My Blog" 
   size={26}
   diameter={12}
